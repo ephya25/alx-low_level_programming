4327bc12eac44cdae49ee0cac171c0d29d82bddc
ephya25 <ephya25@gmail.com>
2023-06-15 20:00:03 -0700

    Quotation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main	- Entry point
+ * main	- print "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19,
+ * on a new line, to the standard error.
  *
- * Return: 0 (properly exited)
+ * Return: 0 (Successful)
  */
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return(1);
+}
