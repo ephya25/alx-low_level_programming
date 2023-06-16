8d5ab98c02c8637e88e140887e2ae269034321fc
ephya25 <ephya25@gmail.com>
2023-06-15 20:04:31 -0700

    Quotation two

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main	- print "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19,
- * on a new line, to the standard error.
+ * main	- Entry point
  *
  * Return: 0 (Successful)
  */
