f375e6d92fcc84083c7cf4c3224182de3c078ae7
ephya25 <ephya25@gmail.com>
2023-06-15 17:08:15 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print "Programming is like building a multilingual puzzle using puts as function
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
