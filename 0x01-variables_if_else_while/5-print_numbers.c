f55d52655e603cfd980f06baf7bc54a153aebe24
ephya25 <ephya25@gmail.com>
2023-06-16 19:22:14 -0700

    Five numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main	- print all single digit numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%i", i);
+	putchar('\n');
+
+	return (0);
+}
