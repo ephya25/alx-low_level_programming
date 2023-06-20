eb88b66a18c09a5b535c3d5e7485cd79db712eda
ephya25 <ephya25@gmail.com>
2023-06-20 09:51:58 -0700

    Print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,20 @@
  */
 void print_alphabet_x10(void)
 {
-	char i;
-	int j;
+	char ch;
+	int i;
 
-	for (j = 0; j < 10; j++;)
+	i = 0;
+
+	while(i < 10)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
+		ch = 'a';
+		while(ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar(ch);
+		i++;
 	}
 }
