1b096dadf46529d45bb7138df43db676736de812
ephya25 <ephya25@gmail.com>
2023-06-20 18:18:08 -0700

    Times hundred

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,5 @@
-#ifndef MAIN_HEADER_FILE
-#define MAIN_HEADER_FILE
-
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-
-#endif
+104-fibonacci.c
+103-fibonacci.c
+102-fibonacci.c
+101-natural.c
+100-times_table.c
