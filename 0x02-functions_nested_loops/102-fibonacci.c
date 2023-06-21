5b473a8d1b71ecba237a862a03abba1f5356f33d
ephya25 <ephya25@gmail.com>
2023-06-20 19:28:05 -0700

    Fibonacci 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,8 +13,13 @@ int main(void)
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	for (i = 2; i < 50; i++)
+	for (i = 2; i < 50, i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (i == 49)
-			printf(
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
+	}
+	return (0);
+}
