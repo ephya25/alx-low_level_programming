019d1f55716219e5b7acf82ecd76303e3f0c0f2a
ephya25 <ephya25@gmail.com>
2023-06-22 15:02:38 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
  */
 void more_numbers(void)
 {
-	int i, c;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (c = 0; c < 15; c++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (c >= 10)
-				_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
 		}
 		_putchar('\n');
 	}
