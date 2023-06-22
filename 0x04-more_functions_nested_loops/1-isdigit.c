715d1a1b214a111e70fca3ef07fedddcebd41290
ephya25 <ephya25@gmail.com>
2023-06-22 04:00:48 -0700

    Digit c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**_isdigit - function that checks for a digit from 0 to 9
+ *
+ * Return: 1 if c is a digit , otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0)
+	}
+}
