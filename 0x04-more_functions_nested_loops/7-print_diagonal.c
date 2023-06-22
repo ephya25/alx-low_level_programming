a6445c345c259e94c4ba7c88dd76523f38e05546
ephya25 <ephya25@gmail.com>
2023-06-22 15:18:08 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int a, b;
-
-	for (a = 0; a < n; a++)
+	if (n <= 0)
 	{
-		for (b = 0; b < a; b++)
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-		}
-		_putchar(92);
-		if (a < (n-1))
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
