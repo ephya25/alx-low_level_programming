81a71e9e01acb15b5f1b4502d949040aad482b0b
ephya25 <ephya25@gmail.com>
2023-06-22 15:09:54 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,18 @@
  */
 void print_line(int n)
 {
-	int a;
-
-	for (a = 0; a < n; a++)
+	if (n <= 0)
 	{
-		_putchar(95);
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
