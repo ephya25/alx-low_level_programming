613f282738746613feac1b50422143988861302b
ephya25 <ephya25@gmail.com>
2023-06-22 20:25:17 -0700

    Number c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - function that prints an integer
- *
- * Return: nothing
+ * print_number - prints an integer
+ * @n: integer to be printed
  */
 void print_number(int n)
 {
